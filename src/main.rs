c367c8f966c3c657ef0c271b435b5ffd64add76d
tristan-orourke <tristan.orourke@gmail.com>
2025-05-16 01:48:38 -0400

    When hero is created, add log to Notifications resource

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
 use bevy::prelude::*;
 use std::collections::HashMap;
 
-#[derive(Component)]
+#[derive(Resource, Default)]
 struct Turn(u32);
+
+#[derive(Resource, Default)]
+struct Notificiations(Vec<Notification>);
+
+struct Notification {
+    message: String,
+    is_unread: bool,
+}
+
+#[derive(Event)]
+struct NotificationEvent(String);
 
 #[derive(Component)]
 struct Hero;
@@ -13,27 +24,28 @@ struct Person {
     relationships: HashMap<Entity, i32>,
 }
 
+#[derive(Debug)]
 enum Personality {
-    Friendly, // +1 opinion of party members after questing together, regardless of outcome
+    Friendly,       // +1 opinion of party members after questing together, regardless of outcome
     ResultOriented, // +1 opinion of party members if successful, -1 if not
-    Mirror, // Moves toward the other person's opinion of them
-    Judgmental, // -2 opinion of party members if they get injured, +1 otherwise.
-    Learner, // +1 opinion of anyone stronger, -1 of anyone weaker
-    Teacher, // +1 opinion of anyone weaker, -1 of anyone stronger
+    Mirror,         // Moves toward the other person's opinion of them
+    Judgmental,     // -2 opinion of party members if they get injured, +1 otherwise.
+    Learner,        // +1 opinion of anyone stronger, -1 of anyone weaker
+    Teacher,        // +1 opinion of anyone weaker, -1 of anyone stronger
 }
 
-#[derive(Component)]
+#[derive(Component, Debug)]
 enum HeroClass {
     Warrior,
     Tank,
-    Support
+    Support,
 }
 
 #[derive(Component)]
 struct LevelState {
     level: u32,
     exp: u32,
-    exp_to_next: u32
+    exp_to_next: u32,
 }
 
 struct Item {
@@ -77,13 +89,17 @@ struct QuestBundle {
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .init_resource::<Turn>()
+        .init_resource::<Notificiations>()
+        .add_event::<NotificationEvent>()
         .add_systems(Startup, setup)
-        .add_systems(Update, log_once_system)   
+        .add_systems(Update, log_new_hero)
+        .add_systems(Update, handle_notifcation_events)
         .run();
 }
 
 fn setup(mut commands: Commands) {
-    // Setup the game world
+    // Setup some initial heros and quests
     commands.spawn(HeroBundle {
         marker: Hero,
         level: LevelState {
@@ -94,6 +110,19 @@ fn setup(mut commands: Commands) {
         class: HeroClass::Warrior,
         person: Person {
             personality: Personality::Friendly,
+            relationships: HashMap::new(),
+        },
+    });
+    commands.spawn(HeroBundle {
+        marker: Hero,
+        level: LevelState {
+            level: 1,
+            exp: 0,
+            exp_to_next: 100,
+        },
+        class: HeroClass::Tank,
+        person: Person {
+            personality: Personality::ResultOriented,
             relationships: HashMap::new(),
         },
     });
@@ -115,11 +144,30 @@ fn setup(mut commands: Commands) {
     });
 }
 
-fn log_once_system() {
-    // The 'once' variants of each log level are useful when a system is called every frame,
-    // but we still wish to inform the user only once. In other words, use these to prevent spam :)
+fn log_new_hero(
+    query: Query<(&LevelState, &HeroClass, &Person), Added<Hero>>,
+    mut ev_notify: EventWriter<NotificationEvent>,
+) {
+    for (level, class, person) in query.iter() {
+        ev_notify.write(NotificationEvent(format!(
+            "New hero created: Level: {}, Class: {:?}, Personality: {:?}",
+            level.level, class, person.personality
+        )));
+    }
+}
 
-    debug_once!("one time debug message");
+fn handle_notifcation_events(
+    mut ev_notifcations: EventReader<NotificationEvent>,
+    mut notifications: ResMut<Notificiations>,
+) {
+    for event in ev_notifcations.read() {
+        let n = Notification {
+            message: event.0.clone(),
+            is_unread: true,
+        };
+        info!("Notification: {}", n.message);
+        notifications.0.push(n);
+    }
 }
 
 // When TurnDelta event happens, advance Turn resource
